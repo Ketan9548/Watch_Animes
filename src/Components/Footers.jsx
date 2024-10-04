4614422a6b65eeb7d0f32dd4286a7cb8593e921a
kapil <kc918062@gmail.com>
2024-10-04 17:28:09 +0530

    done

--- src/Components/Footers.jsx
+++ src/Components/Footers.jsx
@@ -10,10 +10,10 @@ const Footers = () => {
         </div>
         <div className='flex justify-center'>
           <div className='flex flex-row'>
-            <p className='ml-4 mr-4 hover:font-bold'><NavLink to="">Contact Us</NavLink></p>
-            <p className='ml-4 mr-4 hover:font-bold'><NavLink>About Us</NavLink></p>
-            <p className='ml-4 mr-4 hover:font-bold'><NavLink>Sitemap</NavLink></p>
-            <p className='ml-4 mr-4 hover:font-bold'><NavLink>More</NavLink></p>
+            <p className='ml-4 mr-4 bg-gray-600 pl-2 pr-2 hover:font-bold hover:opacity-70'><NavLink to="">Contact Us</NavLink></p>
+            <p className='ml-4 mr-4 bg-gray-600 pl-2 pr-2 hover:font-bold hover:opacity-70'><NavLink>About Us</NavLink></p>
+            <p className='ml-4 mr-4 bg-gray-600 pl-2 pr-2 hover:font-bold hover:opacity-70'><NavLink>Sitemap</NavLink></p>
+            <p className='ml-4 mr-4 bg-gray-600 pl-2 pr-2 hover:font-bold hover:opacity-70'><NavLink>More</NavLink></p>
           </div>
         </div>
       </div>
